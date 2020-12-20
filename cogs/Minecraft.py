9033c8bb6e1db858d0b53b80e9c6bd35378031c1
Collin Sparks <battalionmouse@gmail.com>
2020-12-20 13:56:54 -0600

    -Fixed lots of bugs in Minecraft cog. Ready for first deployment.

--- cogs/Minecraft.py
+++ cogs/Minecraft.py
@@ -5,8 +5,10 @@
 import discord
 from discord.ext import commands
 import asyncio
-from mcstatus
-            
+import mcstatus
+
+blueServer = {'host': 'ca2.villagerhost.net',
+            'port': 25601 }
 
 class Minecraft(commands.Cog):
 
@@ -14,43 +16,46 @@ class Minecraft(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def playercount(self, ctx, getList=False):
+    async def playercount(self, ctx, getNames=None): #getNames is an optional arguement to return the list of playernames
         
-        await ctx.channel.send(buildQueryReturn(queryServer(), getList))
+        await ctx.channel.send(buildQueryReturn(queryServer(), getNames))
 
 
             
-def writeError(failstep)
-    errormsg = 'Server {} failed :(\n'.format(failstep) +
-            '@{} fix your shit!'.format('259610120294498304')) #this is my discord user ID
+def writeError(failstep):
+    errormsg = ('Server {} failed :(\n'.format(failstep) +
+            '<@259610120294498304> fix your shit!'.format('')) #this is my discord user ID
     return errormsg
 
 
 def queryServer():
     errorcode = 0
+    result = None
     try:
-        server = mcstatus.MinecraftServer('ca2.villagerhost.net',25601)
+        server = mcstatus.MinecraftServer(blueServer['host'], blueServer['port'])
     except:
         errorcode = 1
         result = writeError('lookup')
+        return (errorcode, result) #if something goes wrong, queryTuple[1] is the error message
         
     try:
         query = server.query()
     except:
         errorcode = 1
         result = writeError('query')
+        return (errorcode, result)
 
-    return (errorcode, result)
+    return (errorcode, query) #if everything works, then queryTuple[1] is the query object
 
-def buildQueryReturn(queryTuple, getList):
-    if queryTuple[0] = 1: #something went wrong
+def buildQueryReturn(queryTuple, getNames):
+    if queryTuple[0] == 1: #something went wrong
         return queryTuple[1] #the error message
     else:
-        playerlist = query.players.names
-        playercount = len(playerlist)
-        message = 'There are currently {} players connected to the Baby Blue Minecraft server'.format(playercount)AssertionError
-        if getList == 'list':
-            message = message + "\n\nHere are the connected players:\n{}".format('\n'.join(playerlist))
+        playernames = queryTuple[1].players.names #queryTuple[1] is the query object
+        playercount = len(playernames)
+        message = 'There are currently {} players connected to the Baby Blue Minecraft server'.format(playercount)
+        if getNames == 'names': #optional argument 'names' from the original command
+            message = message + "\n\nHere are the connected players:\n{}".format('\n'.join(playernames))
 
         return message
 
@@ -58,6 +63,8 @@ def buildQueryReturn(queryTuple, getList):
 
         
 
+def setup(bot):
+    bot.add_cog(Minecraft(bot))
 
 
     
