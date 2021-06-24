6dacced0d059ceadcc9b37d5399ee1ebcc0959e2
Collin Sparks <cklsparks@gmail.com>
2021-06-24 15:59:13 -0500

    completed Collection and ColxItem basic functionality

--- cogs/Listkeeper.py
+++ cogs/Listkeeper.py
@@ -1,7 +1,7 @@
 # Cog to keep track of lists made by users
 
-import discord
-from discord.ext import commands
+# import discord
+# from discord.ext import commands
 import asyncio
 import os
 import json
@@ -12,36 +12,69 @@ from typing import Union, Dict, List
 ## CLASS DEFS ##
 class ColxItem():
 
-
-
     def __init__(self, label:str, note:str="") -> None:
         self.label: str = label
         self.note: str = note
 
 
+    def __repr__(self) -> str:
+        return f"(Item) {self.label}"
+
+
+    def set_label(self, label:str) -> None:
+        self.label = label
+
+
+    def set_note(self, note:str) -> None:
+        self.note = note
+
+
+    def printout(self) -> str:
+        return f"{self.label}: {self.note}"
+
+
+
 class Collection():
 
     master: List[ColxItem] = []
-    selected_list: Union[ColxItem, None] = None
+    selected_list: Union[List[ColxItem], None] = None
 
     def __init__(self, name, desc) -> None:
         self.name: str = name
         self.desc: str = desc
         self.contents: List[ColxItem] = []
 
+        Collection.master.append(self)
+        Collection.selected_list = self
 
-    def delete(self) -> None:
+
+    def __repr__(self) -> str:
+        return f"(Collection Object) {self.name}: {self.desc}"
+
+
+    def delete_self(self) -> None:
         print(f'Removing {self.name} from Listkeeper!')
         Collection.master.remove(self)
+        if Collection.selected_list == self:
+            Collection.selected_list = None
 
 
-    def add_item(self, label:str, note:str) -> None: #
+    def add_item(self, label:str, note:str="") -> None: #
+        for item in self.contents:
+            if item.label == label:
+                print("ERROR: Label already in use!")
+                return
+
         item: ColxItem = ColxItem(label, note)
         self.contents.append(item)
         save_to_file()
 
 
-
+    def delete_item(self, label:str) -> None:
+        for item in self.contents:
+            if item.label == label:
+                print("removing", item.label)
+                self.contents.remove(item)
 
 
 
@@ -54,25 +87,25 @@ def save_to_file() -> None:
     pass
 
 
-## MAIN COG ##
-class ListKeeper(commands.cog):
+# ## MAIN COG ##
+# class ListKeeper(commands.cog):
 
-    def __init__(self, bot) -> None:
-        self.bot = bot
-
-
-    def dir_init(self, ctx) -> None:
-        # check for directory exists
-        # if os.path.isdir(str(ctx.message.guild.id)):
-        #   ctx.channel.send('All set up!')
-        # else:
-        #   ctx.channel.send('Setting up folder!')
-        #   make folder
-        pass
+#     def __init__(self, bot) -> None:
+#         self.bot = bot
 
 
-    @commands.command()
-    def list(self, ctx, colx_name=None) -> None:
-        # if not colx_name: return [self.name for name in List.master]
-        # else: 1)Find where List.master.name == colx_name and return that obj; 2)Return that obj info
-        pass
+#     def dir_init(self, ctx) -> None:
+#         # check for directory exists
+#         # if os.path.isdir(str(ctx.message.guild.id)):
+#         #   ctx.channel.send('All set up!')
+#         # else:
+#         #   ctx.channel.send('Setting up folder!')
+#         #   make folder
+#         pass
+
+
+#     @commands.command()
+#     def list(self, ctx, colx_name=None) -> None:
+#         # if not colx_name: return [self.name for name in List.master]
+#         # else: 1)Find where List.master.name == colx_name and return that obj; 2)Return that obj info
+#         pass
