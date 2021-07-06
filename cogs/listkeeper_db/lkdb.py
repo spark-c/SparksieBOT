80712b5340ea9d00b9753f4508db53ac0e91681b
Collin Sparks <cklsparks@gmail.com>
2021-07-06 14:09:23 -0500

    lkdb.py now connects to db, creates tables, basic models

--- cogs/listkeeper_db/lkdb.py
+++ cogs/listkeeper_db/lkdb.py
@@ -1,22 +1,30 @@
 # manages interfacing with heroku postgres db for ListKeeper cog
-from sqlalchemy.orm import declarative_base, sessionmaker
-from sqlalchemy import declarative_base, create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String
 
-engine = create_engine('sqlite:///:memory:', echo=False)
+
+engine = create_engine('postgresql://localhost/baby-bot-dev', echo=False)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
+
 class Collection(Base):
+    __tablename__ = "collection"
 
     name = Column(String, nullable=False)
     description = Column(String, nullable=True)
     collection_id = Column(Integer, primary_key=True, nullable=False)
-    items = relationship("Items", backref="Collection", cascade="all, delete-orphan")
+    items = relationship("Item", backref="collection", cascade="all, delete-orphan")
 
 
-class Items(Base):
+class Item(Base):
+    __tablename__ = "item"
 
     name = Column(String, nullable=False)
     note = Column(String, nullable=True)
     item_id = Column(Integer, primary_key=True, nullable=False)
-    collection_id = Column(Integer, ForeignKey("Collection.collection_id"), nullable=False)
+    collection_id = Column(Integer, ForeignKey("collection.collection_id"), nullable=False)
+    # collection = relationship("Collection", back_populates="items")
+
+
+Base.metadata.create_all(engine)
