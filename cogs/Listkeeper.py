6c5302c52c3ebf62e761796f2236124f7ef7397c
Collin Sparks <cklsparks@gmail.com>
2021-07-11 12:05:15 -0500

    skeletoned command functions

--- cogs/Listkeeper.py
+++ cogs/Listkeeper.py
@@ -9,4 +9,68 @@ import listkeeper_db.lkdb as lkdb
 
 
 class Listkeeper(commands.cog):
-    pass
+    
+
+    ## Create
+    @commands.command
+    def newlist(ctx) -> None:
+        pass
+
+
+    @commands.command
+    def additem(ctx) -> None:
+        pass
+
+
+    ## Read
+    @commands.command
+    def listall(ctx) -> None:
+        pass
+
+
+    @commands.command
+    def list(ctx) -> None:
+        pass
+
+
+    ## Update
+    @commands.command
+    def updateitem(ctx) -> None:
+        pass
+
+
+    @commands.command
+    def updatelist(ctx) -> None:
+        pass
+
+
+    # Delete
+    @commands.command
+    def rmlist(ctx) -> None:
+        pass
+
+
+    @commands.command
+    def rmitem(ctx) -> None:
+        pass
+
+
+    # Utility
+    @commands.command
+    def listundo(ctx) -> None:
+        pass
+
+
+
+
+    
+
+
+    
+
+
+    
+    
+
+
+    
