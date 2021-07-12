ef1fbb596c11a2c9104fa10bf95468d978979d3c
Collin Sparks <cklsparks@gmail.com>
2021-07-12 14:51:28 -0500

    implemented newlist() and listall()

--- cogs/Listkeeper.py
+++ cogs/Listkeeper.py
@@ -1,5 +1,6 @@
 # Cog to keep track of lists made by users
 
+from cogs.listkeeper_db.lkdb import Collection
 import discord
 from discord.ext import commands
 import asyncio
@@ -10,54 +11,65 @@ import listkeeper_db.lkdb as lkdb
 
 class Listkeeper(commands.cog):
     
+    selected_list: Union[lkdb.Collection, None] = None
 
     ## Create
     @commands.command
-    def newlist(ctx) -> None:
-        pass
+    def newlist(self, ctx, *args) -> None:
+        name = args[0]
+        desc = args[1] if args[1] else None
+        new_colx = lkdb.create_collection(
+            name=name,
+            description=desc,
+            collection_id=lkdb.generate_id(),
+            guild_id=ctx.guild.id
+        )
+        Listkeeper.selected_list = new_colx
 
 
     @commands.command
-    def additem(ctx) -> None:
+    def additem(self, ctx) -> None:
         pass
 
 
     ## Read
     @commands.command
-    def listall(ctx) -> None:
-        pass
+    def listall(self, ctx) -> None:
+        tmp: List[Collection] = lkdb.get_guild_collections(ctx.guild.id)
+        collection_names: List[str] = [colx.name for colx in tmp]
+        ctx.channel.send(f"Here are your lists:\n{collection_names.join('\n')}")
 
 
     @commands.command
-    def list(ctx) -> None:
+    def list(self, ctx) -> None:
         pass
 
 
     ## Update
     @commands.command
-    def updateitem(ctx) -> None:
+    def updateitem(self, ctx) -> None:
         pass
 
 
     @commands.command
-    def updatelist(ctx) -> None:
+    def updatelist(self, ctx) -> None:
         pass
 
 
     # Delete
     @commands.command
-    def rmlist(ctx) -> None:
+    def rmlist(self, ctx) -> None:
         pass
 
 
     @commands.command
-    def rmitem(ctx) -> None:
+    def rmitem(self, ctx) -> None:
         pass
 
 
     # Utility
     @commands.command
-    def listundo(ctx) -> None:
+    def listundo(self, ctx) -> None:
         pass
 
 
