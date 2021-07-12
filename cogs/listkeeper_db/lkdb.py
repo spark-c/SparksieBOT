253957fbdb21f5e67ceb7fe061ef2c8fa7908a4b
Collin Sparks <cklsparks@gmail.com>
2021-07-12 12:37:40 -0500

    changed default arg implementation and improved DatabaseError message

--- cogs/listkeeper_db/lkdb.py
+++ cogs/listkeeper_db/lkdb.py
@@ -2,7 +2,15 @@
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 from sqlalchemy import create_engine, ForeignKey, Column, Integer, String
 
-from typing import Union, List
+from typing import Union, List, Any
+
+class DatabaseError(Exception):
+    def __init__(self, message="Unable to complete database operation!", object_to_debug: Any=None) -> None:
+        # self.object_debug: str = object_to_debug.debug()
+        # self.message: str = message + "\n" + self.object_debug
+        self.message: str = message
+        super().__init__(self.message)
+
 
 
 # TODO: handle possibility of db downtime
@@ -16,7 +24,7 @@ class Collection(Base):
     __tablename__ = "collection"
 
     name = Column(String, nullable=False)
-    description = Column(String, nullable=True)
+    description = Column(String, nullable=False)
     collection_id = Column(Integer, primary_key=True, nullable=False)
     items = relationship("Item", backref="collection", cascade="all, delete-orphan")
     #TODO guild_id = Column(Integer, nullable=False)
@@ -33,12 +41,31 @@ class Item(Base):
 
 
 Base.metadata.create_all(engine)
+session = Session()
 
 ## HELPER FUNCTIONS ##
 
 ## Create
-def create_collection(name: str, collection_id: int, guild_id: int, description: str="") -> None:
-    pass
+def create_collection(name: str, description: str, collection_id: int, guild_id: int) -> Union[Collection, None]:
+    description = "" if description == None else description # default argument "" (conditional expression)
+    new_colx = Collection(name=name, description=description, collection_id=collection_id, guild_id=guild_id)
+    with Session() as session:
+        try:
+            session.add(new_colx)
+            session.commit()
+        except:
+            raise DatabaseError(
+                "Failed to add new entry to database!\n" +
+                "new_colx\n" +
+                f"name: {name}\n" +
+                f"description: {description}\n" +
+                f"collection_id: {collection_id}\n" +
+                f"guild_id: {guild_id}\n"
+            )
+
+        ## Commented the below so we will fail loudly during development
+        # with session.begin(): # SessionTransaction object; automatically commit()s at end of context block if no exceptions.
+        #     session.add(new_colx)
 
 
 def create_item(name: str, item_id: int, collection_id: int, note: str="") -> None:
