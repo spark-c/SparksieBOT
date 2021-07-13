d529ab385c58f873269ce3a3c44291838cc7f173
Collin Sparks <cklsparks@gmail.com>
2021-07-13 18:23:49 -0500

    implemented additem(), untested

--- cogs/Listkeeper.py
+++ cogs/Listkeeper.py
@@ -8,7 +8,7 @@ import asyncio
 from typing import Union, Dict, List
 
 try:
-    from cogs.listkeeper_db.lkdb import Collection
+    from cogs.listkeeper_db.lkdb import Collection, Item
     import cogs.listkeeper_db.lkdb as lkdb
 except Exception as e:
     print(e)
@@ -27,7 +27,7 @@ class Listkeeper(commands.Cog):
     async def newlist(self, ctx, *args) -> None:
         name: str = args[0]
         desc: Union[str, None] = args[1] if args[1] else None
-        new_colx = lkdb.create_collection(
+        new_colx: Union[Collection, None] = lkdb.create_collection(
             name=name,
             description=desc,
             collection_id=lkdb.generate_id(),
@@ -37,16 +37,54 @@ class Listkeeper(commands.Cog):
             Listkeeper.selected_list = new_colx
             await ctx.channel.send(f"Successfully created {new_colx.name}!")
         else:
-            await ctx.channel.send("Something went wrong!")
+            await ctx.channel.send("Could not create list!")
 
 
     @commands.command()
     async def additem(self, ctx, *args) -> None:
-        pass
-        # if args[0].startswith("-"):
-        #     if args[0].tolower() in ["-l", "-list"]:
-        #         # do work
-        #     else:
+        ## Check if list name is passed
+        if args[0].startswith("-"):
+            # handle misuse
+            if len(args) > 3:
+                await ctx.channel.send("Usage: !additem [-l <listname>] <item-name> [<item-note>]")
+                return
+
+            if args[0].tolower() not in ["-l", "-list"]:
+                await ctx.channel.send(f"Invalid argument: {args[0]}!")
+                return
+            
+            if not args[1]:
+                await ctx.channel.send("Provide a list name: !additem -l <listname>")
+                return
+            
+            # query for a list matching given name
+            found: Collection = lkdb.get_collection_by_name(args[1], str(ctx.guild.id))
+            if not found:
+                await ctx.channel.send(f"No list found with name {args[1]}!")
+                return
+            Listkeeper.selected_list = found
+
+            # clean args
+            args = args[2:]
+
+        ## Create new item
+        if Listkeeper.selected_list is None:
+            await ctx.channel.send("No list selected! !additem -l <list-name>")
+            return
+
+        name: str = args[0]
+        note: str = args[1] if args[1] else ""
+        new_item: Union[Item, None] = lkdb.create_item(
+            name=name,
+            note=note,
+            item_id=lkdb.generate_id(),
+            collection_id=Listkeeper.selected_list.collection_id
+        )
+        if new_item:
+            await ctx.channel.send(f"Successfully created item {new_item.name}")
+        else:
+            await ctx.channel.send(f"Could not create item!")
+
 
             
 
