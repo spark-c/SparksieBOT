8a7b13479e472a3fb17f147dd9bb5125f279dc98
Collin Sparks <cklsparks@gmail.com>
2021-11-01 14:20:35 -0500

    organized and renamed baby_bot.py -> bot.py, added run.py

--- bot.py
+++ bot.py
@@ -1,32 +1,15 @@
 import discord
-import logging
-from discord.ext import commands
 import random
-import asyncio
 import os
 import datetime as dt
 from typing import Union
-
 from discord.ext.commands.bot import Bot
 
 import utils.groovycommands as groovycommands
 
-logger: logging.Logger = logging.getLogger('discord')
-logger.setLevel(logging.DEBUG)
-handler: logging.FileHandler = logging.FileHandler(
-    filename='discord.log',
-    encoding='utf-8',
-    mode='w'
-    )
-handler.setFormatter(
-    logging.Formatter(
-        '%(asctime)s:%(levelname)s:%(name)s: %(message)s'
-        )
-    )
-logger.addHandler(handler)
 
 # Tiny subclass to add a property used for "pausing" the bot
-class SparksieBot(commands.Bot):
+class SparksieBot(Bot):
     def __init__(
         self,
         command_prefix='!',
@@ -49,34 +32,69 @@ class SparksieBot(commands.Bot):
         ctx = await self.get_context(message)
         await self.invoke(ctx)
 
+
 intents = discord.Intents.default()
 intents.members = True
 intents.typing = False
 intents.presences = False
 bot: SparksieBot = SparksieBot(command_prefix='!', intents=intents)
 
-last_glory: dt.datetime = dt.datetime.now() - dt.timedelta(seconds=120)
+
+# --------------------
+# EVENTS
+# --------------------
+
+### ON_READY
 
 @bot.event
 async def on_ready() -> None:
     print('Logged in as {0.user}!'.format(bot))
 
+
+### ON_MESSAGE
+
+last_glory: dt.datetime = dt.datetime.now() - dt.timedelta(seconds=120)
+# TODO: should be per server, not totally global
 @bot.event
 async def on_message(message) -> None:
     global last_glory
 
+    # IGNORE self
     if message.author == bot.user:
         return
 
-    # if someone says glory (with cooldown timer)
+
+    ### RESPONSES
+
+    # GLORY response (with cooldown timer)
     if ('glory' in message.content.lower() and
         (dt.datetime.now() - last_glory).seconds > 120):
         last_glory = dt.datetime.now()
         await message.channel.send("Glory!")
 
-    #if the message is a groovy command
+    # ARMA channel response
+    if message.channel.name == 'arma-discussion': 
+        if random.randint(1,100) < 3:
+            await message.channel.send('USAF is still watching.')
+
+    # SASS to groovy
+    if 'groovy' in message.content.lower():
+        await message.channel.send('...')
+
+    # CONGRATULATIONS
+    if 'congrat' in message.content.lower():
+        for i in range(0,2):
+            await message.channel.send(
+                ':confetti_ball::confetti_ball::confetti_ball:\nCONGRATULATIONS!!!'
+            )
+
+
+    ### LITTERING
+
+    # GROOVY/MUSIC commands litter
     if (message.content.lower().startswith('-') and
     message.channel.name != 'groovybot-corner'):
+    # TODO: set music channel as config item via CHANNEL-ID
         for command in groovycommands.groovycommands:
             if message.content.lower().startswith(command):
                 await message.delete()
@@ -95,25 +113,14 @@ async def on_message(message) -> None:
                         f"{message.author.mention} Where it belongs."
                     )
 
-    #Checks if the message was sent by Groovy / in the wrong channel
+    # GROOVYBOT litter
+    # TODO: set musicbot identity via config items
     if (str(message.author) == 'Groovy#7254' and
     message.channel.name != 'groovybot-corner'): 
         await message.delete()
 
-    if 'groovy' in message.content.lower():
-        await message.channel.send('...')
-
-    if 'congrat' in message.content.lower():
-        for i in range(0,2):
-            await message.channel.send(
-                ':confetti_ball::confetti_ball::confetti_ball:\nCONGRATULATIONS!!!'
-            )
-
-    if message.channel.name == 'arma-discussion':
-        counter: int = random.randint(1,100)
-        if counter < 3:
-            await message.channel.send('USAF is still watching.')
-
+    
+    ### UNPAUSE
     try:
         if message.content.startswith("!unpause"):
             await bot.process_commands(message)
@@ -124,6 +131,10 @@ async def on_message(message) -> None:
     except:
         return
 
+
+# --------------------
+# COMMANDS, FUNCTIONS
+# --------------------
 
 @bot.command()
 async def pause(ctx) -> None:
@@ -172,7 +183,9 @@ async def my_unload(ctx, extension, bot) -> None:
         await ctx.channel.send('Unload \'{0}\' failed!'.format(extension))
 
 
-print('working directory is {0}'.format(os.getcwd()))
+# --------------------
+# LOAD COGS
+# --------------------
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
@@ -184,10 +197,3 @@ for filename in os.listdir('./cogs'):
             print('cog ' + filename + ' failed!')
             print(e)
 
-#For use when running from a real filesystem
-try:
-    with open(r'./baby-bot-token.txt', 'r') as f:
-        token: str = f.read()
-    bot.run(token)
-except: #For use when deployed via heroku (using config vars to feed in the token)
-    bot.run(os.environ["TOKEN"])
