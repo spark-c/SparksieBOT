ddb1b98cff2ca93a2cc8502b5ed861548eac8f29
Collin Sparks <cklsparks@gmail.com>
2021-11-14 15:41:19 -0600

    temporarily commented out new factory code

--- tests/test_Listkeeper.py
+++ tests/test_Listkeeper.py
@@ -1,17 +1,17 @@
-import pytest
-import discord.ext.test as dpytest
-from discord import Embed
-import random
-import requests
-from . import conftest
+# import pytest
+# import discord.ext.test as dpytest
+# from discord import Embed
+# import random
+# import requests
+# from . import conftest
 
 
-@pytest.fixture
-def cog_bot(bot):
-    bot_with_cog = bot(["Listkeeper"])
-    return bot_with_cog
+# @pytest.fixture
+# def cog_bot(bot):
+#     bot_with_cog = bot(["Listkeeper"])
+#     return bot_with_cog
 
 
-@pytest.mark.asyncio
-async def test_create_new_list(self, cog_bot):
-    assert True
+# @pytest.mark.asyncio
+# async def test_create_new_list(self, cog_bot):
+#     assert True
