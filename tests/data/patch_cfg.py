79318adc09600fd741a715c6fcb3a0b17335c61a
Collin Sparks <cklsparks@gmail.com>
2021-11-16 18:12:12 -0600

    added basic test for default !pspop

--- tests/data/patch_cfg.py
+++ tests/data/patch_cfg.py
@@ -16,5 +16,39 @@ lookup = {
     "https://next": {
         "another": "thing",
         "to": "return"
+    },
+
+    #--------------------
+    # !pspop
+    #--------------------
+    "https://ps2.fisu.pw/api/population/?world=17": {
+        "result": [
+            {
+                "vs": "99",
+                "nc": "99",
+                "tr": "99",
+                "ns": "99"
+            }
+        ]
+    },
+    "https://ps2.fisu.pw/api/population/?world=1": {
+        "result": [
+            {
+                "vs": "100",
+                "nc": "100",
+                "tr": "100",
+                "ns": "100"
+            }
+        ]
+    },
+    "https://ps2.fisu.pw/api/population/?world=10": {
+        "result": [
+            {
+                "vs": "101",
+                "nc": "101",
+                "tr": "101",
+                "ns": "101"
+            }
+        ]
     }
 }
---
+++ tests/test_ApiCalls.py
@@ -0,0 +1,50 @@
+import pytest
+import discord.ext.test as dpytest
+from discord import Embed
+from . import conftest
+
+
+@pytest.fixture
+def cog_bot(bot):
+    bot_with_cog = bot(["ApiCalls"])
+    return bot_with_cog
+
+
+@pytest.mark.asyncio
+async def test_pspop_default(cog_bot, patched_request):
+    await dpytest.message("!pspop")
+    assert dpytest.verify().message().content(
+        "Here is the current population for Emerald:\n" +
+        "VS: 99\n" +
+        "NC: 99\n" +
+        "TR: 99\n" +
+        "NSO: 99"
+    )
+
+
+@pytest.mark.parametrize(
+    "args, server",
+    [
+        ("--server connery", "connery"),
+        ("-s connery", "connery"),
+        ("-s miller", "miller")
+    ]
+)
+@pytest.mark.asyncio
+async def test_pspop_server_flag(cog_bot, patched_request, args, server):
+    # await dpytest.message(f"!pspop {args}")
+    # assert dpytest.verify().message().contains().content(server)
+    assert True
+
+
+
+@pytest.mark.asyncio
+async def test_pspop_argument_error(cog_bot, patched_request_404):
+    # TODO
+    assert True
+
+
+@pytest.mark.asyncio
+async def test_pspop_network_error(cog_bot, patched_request_404):
+    # TODO
+    assert True
